8961332859d5def0caf05767c3ecee9b7d27c9e7
Pavel Shatunov <shatuman@ya.ru>
2023-01-28 19:32:22 +0300

    Initial Commit

---
+++ VKSwiftUI/VKSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VKSwiftUIApp.swift
+//  VKSwiftUI
+//
+//  Created by Павел Шатунов on 28.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct VKSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
